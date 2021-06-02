8266d5a74ac06db7875626a04ef27f96c34063d0
Carson Essabhoy <cjessabhoy@gmail.com>
2021-06-02 15:22:06 -0700

    solved merging conflict

--- SalesVsReviewScore/server.R
+++ SalesVsReviewScore/server.R
@@ -17,11 +17,9 @@ gameGenres <- allGames %>%
 
 shinyServer(function(input, output) {
     
-<<<<<<< HEAD
+    
 ################## Introduction ##########################
     
-=======
->>>>>>> c449046eb53663421049e734274a4df8f565511e
     output$introparagraph <- renderText({
         paste0("In this project the group took a look at a dataset about game sales. This dataset has quite a bit of information, such as the developer, 
                average review of a game, genre, et cetera. We used the data to look how genre affect sales, if sales was corrolated with review score, the differences
@@ -32,13 +30,8 @@ shinyServer(function(input, output) {
         
         
     })
-<<<<<<< HEAD
-=======
-               
     
->>>>>>> c449046eb53663421049e734274a4df8f565511e
-    
-################### Sales Vs Review Scores #######################
+################### Sales Vs Review Scores #######################S
     
     output$reviewPlot <- renderPlot({
 
